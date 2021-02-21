4266ec555237a621f103928c12684b93860cee9c
Keven S. Lopes <kstephanl1908@gmail.com>
2021-02-20 23:26:53 -0300

    Adicionando codigo inicial

---
+++ app.js
@@ -0,0 +1,9 @@
+import { createMatrixA, getMatrixAValue } from './src/matrixA.js'
+import { createMatrixB, getMatrixBValue } from './src/matrixB.js'
+import { createMatrixC, getMatrixCValue } from './src/matrixC.js'
+import { createMatrixD, getMatrixDValue } from './src/matrixD.js'
+
+console.log(createMatrixA());
+console.log(createMatrixB());
+console.log(createMatrixC());
+console.log(createMatrixD());
