92dc9fe09319d6059f1e6c12a080f686c4019d57
Keven S. Lopes <kstephanl1908@gmail.com>
2021-02-23 03:18:25 -0300

    Adicionando funçao multiply

--- src/exercise/app.js
+++ src/exercise/app.js
@@ -1,5 +1,6 @@
 import { sum } from './src/exerciseA.js';
-import { transposed, subtraction, multiply } from './src/exerciseB.js';
+import { transposed, subtraction, multiplyByTwo } from './src/exerciseB.js';
+import { multiply } from './src/exerciseC.js';
 import { createMatrixA } from '../matrixA.js';
 import { createMatrixB } from '../matrixB.js';
 import { createMatrixC } from '../matrixC.js';
@@ -9,4 +10,5 @@ const matrixA = createMatrixA();
 const matrixB = createMatrixB();
 
 export const letterA = sum(matrixA, matrixB);
-export const letterB = subtraction(transposed(matrixA), multiply(matrixB));
+export const letterB = subtraction(transposed(matrixA), multiplyByTwo(matrixB));
+export const letterC = multiply(matrixA,matrixB);
