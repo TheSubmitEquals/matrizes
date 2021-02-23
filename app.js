7543224519de58af4235a24066f54ba2d672f15b
Keven S. Lopes <kstephanl1908@gmail.com>
2021-02-23 03:17:35 -0300

    Adicionando Letra C no App.js

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import { createMatrixA } from './src/matrixA.js'
 import { createMatrixB } from './src/matrixB.js'
 import { createMatrixC } from './src/matrixC.js'
 import { createMatrixD } from './src/matrixD.js'
-import { letterA, letterB } from './src/exercise/app.js';
+import { letterA, letterB, letterC } from './src/exercise/app.js';
 
 console.log(createMatrixA());
 console.log(createMatrixB());
 console.log(createMatrixC());
 console.log(createMatrixD());
 
-console.log(letterA)
-console.log(letterB)
+console.log(letterA);
+console.log(letterB);
+console.log(letterC);
