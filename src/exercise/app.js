8d518ad64d5a15e06c6a7d2815b7ade99fa981f8
Keven S. Lopes <kstephanl1908@gmail.com>
2021-02-25 03:43:51 -0300

    Adicionando exercicio 1 completo, com todas as letras

--- src/exercise/app.js
+++ src/exercise/app.js
@@ -1,14 +1,12 @@
-import { sum } from './src/exerciseA.js';
-import { transposed, subtraction, multiplyByTwo } from './src/exerciseB.js';
-import { multiply } from './src/exerciseC.js';
-import { createMatrixA } from '../matrixA.js';
-import { createMatrixB } from '../matrixB.js';
-import { createMatrixC } from '../matrixC.js';
-import { createMatrixD } from '../matrixD.js';
+import { matrixA } from '../matrixA.js';
+import { matrixB } from '../matrixB.js';
+import { matrixC } from '../matrixC.js';
+import { matrixD } from '../matrixD.js';
+import { Matrix } from './matrix.js';
 
-const matrixA = createMatrixA();
-const matrixB = createMatrixB();
-
-export const letterA = sum(matrixA, matrixB);
-export const letterB = subtraction(transposed(matrixA), multiplyByTwo(matrixB));
-export const letterC = multiply(matrixA,matrixB);
+export const letterA = matrixA.sum(matrixA, matrixB);
+export const letterB = matrixA.subtration(new Matrix(matrixA.line, matrixB.column, matrixA.transposed(matrixA)), new Matrix(matrixB.line, matrixB.column, matrixB.multiplyByNumber(2)));
+export const letterC = matrixA.multiply(matrixA, matrixB);
+export const letterD = matrixC.multiply(new Matrix(matrixC.line, matrixC.column, matrixC.multiplyByNumber(2)), matrixD);
+export const letterE = matrixB.multiply(matrixB, new Matrix(matrixD.column, matrixD.line, matrixD.transposed(matrixD)));
+export const letterF = matrixA.transposed(new Matrix(matrixA.line, matrixC.line, matrixA.multiply(new Matrix(matrixA.column, matrixA.line, matrixA.transposed(new Matrix(matrixA.line, matrixA.column, matrixA.multiplyByNumber(2)))),new Matrix(matrixC.column, matrixC.line, matrixC.transposed(new Matrix(matrixC.line, matrixC.column, matrixC.multiplyByNumber(3)))))));
