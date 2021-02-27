e10443246127af9f53ee284dc19be38cad475ca6
Keven S. Lopes <kstephanl1908@gmail.com>
2021-02-26 22:59:45 -0300

    Adicionando jquery que faz a leitura do json e chama a funçao para obter a resposta do exercicio 2

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { matrixB } from './matrixB.js';
 import { matrixC } from './matrixC.js';
 import { matrixD } from './matrixD.js';
 import { Matrix } from './matrix.js';
+import { squareMatrix } from './squareMatrix.js';
 
 export const letterA = matrixA.sum(matrixA, matrixB);
 export const letterB = matrixA.subtration(new Matrix(matrixA.line, matrixB.column, matrixA.transposed(matrixA)), new Matrix(matrixB.line, matrixB.column, matrixB.multiplyByNumber(2)));
@@ -10,3 +11,7 @@ export const letterC = matrixA.multiply(matrixA, matrixB);
 export const letterD = matrixC.multiply(new Matrix(matrixC.line, matrixC.column, matrixC.multiplyByNumber(2)), matrixD);
 export const letterE = matrixB.multiply(matrixB, new Matrix(matrixD.column, matrixD.line, matrixD.transposed(matrixD)));
 export const letterF = matrixA.transposed(new Matrix(matrixA.line, matrixC.line, matrixA.multiply(new Matrix(matrixA.column, matrixA.line, matrixA.transposed(new Matrix(matrixA.line, matrixA.column, matrixA.multiplyByNumber(2)))),new Matrix(matrixC.column, matrixC.line, matrixC.transposed(new Matrix(matrixC.line, matrixC.column, matrixC.multiplyByNumber(3)))))));
+
+$.getJSON('./src/squareMatrix.json', function(jd) {
+    console.log(squareMatrix(new Matrix(jd.matrixOne.length, jd.matrixOne[0].length, jd.matrixOne), new Matrix(jd.matrixTwo.length, jd.matrixTwo[0].length, jd.matrixTwo)));
+})
